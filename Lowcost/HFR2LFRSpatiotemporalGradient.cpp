a3ae077a1cc4d51dfee3c9731ed83c43186d32fc
Karamaz0V1 <florent.guiotte@gmail.com>
2016-01-02 13:39:57 +0100

    Temporal sobel average

--- Lowcost/HFR2LFRSpatiotemporalGradient.cpp
+++ Lowcost/HFR2LFRSpatiotemporalGradient.cpp
@@ -21,11 +21,18 @@ HFR2LFRSpatiotemporalGradient::~HFR2LFRSpatiotemporalGradient()
 
 void HFR2LFRSpatiotemporalGradient::processFrame()
 {
-    cout << "F CP" << endl;
+    // Reduce the noise (3x3 kernel)
     GaussianBlur(m_nextFrame, m_nextFrame, Size(3, 3), 0, 0, BORDER_DEFAULT);
     GaussianBlur(m_currentFrame, m_currentFrame, Size(3, 3), 0, 0, BORDER_DEFAULT);
-	m_finalFrameBGR = Mat(m_height, m_width, CV_8UC3);
-    m_finalFrameBGR = m_nextFrame - m_currentFrame + 127;
+
+    // Compute x sobel gradient
+    Mat currentGradientX, nextGradientX;
+    Sobel(m_currentFrame, currentGradientX, CV_16S, 1, 0, 3);
+    Sobel(m_nextFrame, nextGradientX, CV_16S, 1, 0, 3);
+    convertScaleAbs(currentGradientX, currentGradientX);
+    convertScaleAbs(nextGradientX, nextGradientX);
+    addWeighted(currentGradientX, 0.5, nextGradientX, 0.5, 0, m_finalFrameBGR);
+    //m_finalFrameBGR = m_nextFrame - m_currentFrame + 127;
     imshow("TEMP Player", m_finalFrameBGR);
     waitKey(0);
 }
--- Lowcost/HFR2LFRTemporalGradient.cpp
+++ Lowcost/HFR2LFRTemporalGradient.cpp
@@ -21,7 +21,6 @@ HFR2LFRTemporalGradient::~HFR2LFRTemporalGradient()
 
 void HFR2LFRTemporalGradient::processFrame()
 {
-    cout << "F CP" << endl;
     GaussianBlur(m_nextFrame, m_nextFrame, Size(3, 3), 0, 0, BORDER_DEFAULT);
     GaussianBlur(m_currentFrame, m_currentFrame, Size(3, 3), 0, 0, BORDER_DEFAULT);
 	m_finalFrameBGR = Mat(m_height, m_width, CV_8UC3);
